8024da12cd00ba13601784d9ed9400e3c1668539
Stephen Doxsee <sdoxsee@gmail.com>
2019-09-14 15:15:26 -0400

    Try again

--- src/test/java/com/example/fooapp/FooAppApplicationTests.java
+++ src/test/java/com/example/fooapp/FooAppApplicationTests.java
@@ -1,19 +1,40 @@
 package com.example.fooapp;
 
-import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-//https://stackoverflow.com/a/49011982/1098564
-@AutoConfigureEmbeddedDatabase
+@ContextConfiguration(initializers = {FooAppApplicationTests.Initializer.class})
 public class FooAppApplicationTests {
+
+	@ClassRule
+	public static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres")
+			.withDatabaseName("postgres")
+			.withUsername("integrationUser")
+			.withPassword("testPass");
 
 	@Test
 	public void contextLoads() {
+	}
+
+	static class Initializer
+			implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			TestPropertyValues.of(
+					"spring.datasource.url=" + postgres.getJdbcUrl(),
+					"spring.datasource.username=" + postgres.getUsername(),
+					"spring.datasource.password=" + postgres.getPassword()
+			).applyTo(configurableApplicationContext.getEnvironment());
+		}
 	}
 
 }
