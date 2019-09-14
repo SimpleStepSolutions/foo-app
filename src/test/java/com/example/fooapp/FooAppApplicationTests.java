e821dd71e234af4518cb6c46f22ea60f1fe706ed
Stephen Doxsee <sdoxsee@gmail.com>
2019-09-14 15:05:10 -0400

    Add embedded pg support

--- src/test/java/com/example/fooapp/FooAppApplicationTests.java
+++ src/test/java/com/example/fooapp/FooAppApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.fooapp;
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +8,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+//https://stackoverflow.com/a/49011982/1098564
+@AutoConfigureEmbeddedDatabase
 public class FooAppApplicationTests {
 
 	@Test
